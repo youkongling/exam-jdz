d6d5fe620437a133424d97f505dc294460c8ea07
xiaobo <xiaobo@gmail.com>
2018-03-21 14:22:56 +0800

    修改

--- config/config.js
+++ config/config.js
@@ -1,41 +1,17 @@
-// var config = {
-//     list: 'http://gaojun.s1.natapp.cc/nongshang/admin/index.php?api-app-api',
-//     anwser: 'http://gaojun.s1.natapp.cc/nongshang/admin/index.php?api-app-api-getAnswer',
-//     test: 'http://gaojun.s1.natapp.cc/nongshang/admin/index.php?api-app-api-getQuesList',
-//     user: 'http://gaojun.s1.natapp.cc/nongshang/admin/index.php?api-app-user',
-//     score: 'http://gaojun.s1.natapp.cc/nongshang/admin/index.php?api-app-user-getUserExamList'
-// }
-
-var config = {
+ var config = {
     
-    list: 'http://220.176.196.12/index.php?api-app-api',
-    anwser: 'http://220.176.196.12/index.php?api-app-api-getAnswer',
-    test: 'http://220.176.196.12/index.php?api-app-api-getQuesList',
-    user: 'http://220.176.196.12/index.php?api-app-user',
-    score: 'http://220.176.196.12/index.php?api-app-user-getUserExamList',
-    confirm: 'http://220.176.196.12/index.php?api-app-user-checkUser',
-    openid: 'http://220.176.196.12/index.php?api-app-user-checkUserByOpenId&openid=',
-    knows: 'http://220.176.196.12/index.php?api-app-knows-getKnows',
-    search: 'http://220.176.196.12/index.php?api-app-knows-searchKnows',
-    rank: 'http://220.176.196.12/index.php?api-app-user-getRanking',
-    scoreView: 'http://220.176.196.12/index.php?api-app-api-getAllList'
-}
-
-
-// var config = {
-    
-//     list: 'http://211.159.186.84/index.php?api-app-api',
-//     anwser: 'http://211.159.186.84/index.php?api-app-api-getAnswer',
-//     test: 'http://211.159.186.84/index.php?api-app-api-getQuesList',
-//     user: 'http://211.159.186.84/index.php?api-app-user',
-//     score: 'http://211.159.186.84/index.php?api-app-user-getUserExamList',
-//     confirm: 'http://211.159.186.84/index.php?api-app-user-checkUser',
-//     openid: 'http://211.159.186.84/index.php?api-app-user-checkUserByOpenId&openid=',
-//     knows: 'http://211.159.186.84/index.php?api-app-knows-getKnows',
-//     search: 'http://211.159.186.84/index.php?api-app-knows-searchKnows',
-//     rank: 'http://211.159.186.84/index.php?api-app-user-getRanking',
-//     scoreView: 'http://211.159.186.84/index.php?api-app-api-getAllList'
-// }
+     list: 'http://211.159.186.84/index.php?api-app-api',
+     anwser: 'http://211.159.186.84/index.php?api-app-api-getAnswer',
+     test: 'http://211.159.186.84/index.php?api-app-api-getQuesList',
+     user: 'http://211.159.186.84/index.php?api-app-user',
+     score: 'http://211.159.186.84/index.php?api-app-user-getUserExamList',
+     confirm: 'http://211.159.186.84/index.php?api-app-user-checkUser',
+     openid: 'http://211.159.186.84/index.php?api-app-user-checkUserByOpenId&openid=',
+     knows: 'http://211.159.186.84/index.php?api-app-knows-getKnows',
+     search: 'http://211.159.186.84/index.php?api-app-knows-searchKnows',
+     rank: 'http://211.159.186.84/index.php?api-app-user-getRanking',
+     scoreView: 'http://211.159.186.84/index.php?api-app-api-getAllList'
+ }
 
 
 
